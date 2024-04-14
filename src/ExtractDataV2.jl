13c9c36a01159e78cbaa242b363022210173e229
TheHedgehog2000 <jprentic@andrew.cmu.edu>
2024-04-14 11:57:26 -0400

    Added metadata extraction

---
+++ src/ExtractDataV2.jl
@@ -0,0 +1,80 @@
+using JSON: parsefile
+using DataFrames
+using CSV
+
+function extract_matrix(df, i, row_indices, valid_columns)
+    row_index = row_indices[i]
+    if i == length(row_indices)
+        df_subset = df[row_index+2:end, :]
+    else
+        next_row_index = row_indices[i+1]
+        df_subset = df[row_index+2:next_row_index-1, :]
+    end
+    delete!(rename!(df_subset, Symbol.(Vector(df_subset[1,:]))), 1)
+    df_subset = df_subset[:, intersect(valid_columns, names(df_subset))]
+    for (name, col) in pairs(eachcol(df_subset))
+        if ismissing(df_subset[1, name])
+            select!(df_subset, Not(name))
+        end
+    end
+    dropmissing!(df_subset)
+    return df_subset
+end
+
+function determine_filename(pattern)
+    if occursin(r"Read \d+:600", pattern)
+        return "OD600.csv"
+    elseif occursin(r"Read \d+:Lum", pattern)
+        return "lum.csv"
+    elseif occursin(r"Read \d+:485/20,528/20", pattern)
+        return "YFP.csv"
+    elseif occursin(r"Read \d+:620/40,680/30", pattern)
+        return "CY5.csv"
+    else
+        return nothing 
+    end
+end
+
+function main()
+    config = parsefile("experiment_config.json")
+    bulk_data = config["bulk_data"]
+    images_dir = config["images_directory"]
+    
+    for j in eachindex(bulk_data)
+        bulk_data_dir = bulk_data[j][1:end-4] 
+        images_dir = images_dirs[j]
+        if isdir(images_dir)
+            output_dir = images_dir 
+        else
+            if isdir(bulk_data_dir)
+                rm(bulk_data_dir; recursive = true)
+            end
+            mkdir(bulk_data_dir)
+            output_dir = bulk_data_dir
+        end
+        
+        df = DataFrame(CSV.File(bulk_data, header=false))
+        well_names = [string(c, r) for c in 'A':'H', r in 1:12]
+        rows = passmissing(occursin).([r"Read \d+"], df.Column1)
+        rows = [ismissing(x) ? false : x for x in rows]
+        row_indices = findall(rows)
+        
+        # Extract metadata rows and write to metadata.csv
+        if !isempty(row_indices)
+            first_read_index = row_indices[1]
+            metadata_df = df[1:first_read_index-1, :]
+            CSV.write("$output_dir/metadata.csv", metadata_df)
+        end
+        
+        reads = [df[index, :Column1] for index in row_indices]
+        for i in eachindex(row_indices)
+            data_matrix = extract_matrix(df, i, row_indices, well_names)
+            filename = determine_filename(reads[i])
+            if filename != nothing
+                CSV.write("$output_dir/$filename", data_matrix)
+            end
+        end
+    end
+end
+
+main()
